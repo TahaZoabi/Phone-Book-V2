8489855c9a2dc6aa70d72c7d0e59682ab8a3d5a5
Taha Zoabi <zoabi03@gmail.com>
2024-09-24 09:41:39 +0300

    fixed attributes names

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -40,12 +40,12 @@ function Input() {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              stroke-width="2"
+              strokeWidth="2"
               id="search-btn"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M6 18L18 6M6 6l12 12"
               ></path>
             </svg>
--- src/components/RotateButton.jsx
+++ src/components/RotateButton.jsx
@@ -16,9 +16,9 @@ function RotateButton() {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="lucide lucide-rotate-cw"
         >
           <path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8" />
