301574d95404a03c44400541abc8eff49fcbc721
Taha Zoabi <zoabi03@gmail.com>
2024-09-24 10:21:43 +0300

    render contact list

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,13 +1,26 @@
+import { useContext } from "react";
+import { GlobalContext } from "../GlobalContext.jsx";
+import Contact from "./Contact.jsx";
+
 function ContactList() {
+  const { initialState } = useContext(GlobalContext);
+  const contacts = [...initialState];
   return (
     <>
-      <div id="empty-list">
-        <h3>
-          Your Contact List is Empty, <br />
-          Please add some Contacts
-        </h3>
-      </div>
-      <div className="phone-contact-list"></div>
+      {contacts.length === 0 ? (
+        <div id="empty-list">
+          <h3>
+            Your Contact List is Empty, <br />
+            Please add some Contacts
+          </h3>
+        </div>
+      ) : (
+        <div className="phone-contact-list">
+          {contacts.map((contact) => {
+            return <Contact key={contact.PhoneNumber} contact={contact} />;
+          })}
+        </div>
+      )}
     </>
   );
 }
